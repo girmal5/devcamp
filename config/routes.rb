9118d5bf60e4139224db7dd9660505575dc0bd75
girmal <girmal5@hotmail.com>
2018-05-21 13:46:50 +0530

    learnt routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,23 @@
 Rails.application.routes.draw do
   resources :pfolios
-  get 'pages/home'
-  get 'pages/about'
-  get 'pages/contact'
-  
+
+  # get 'pages/about'
+  # get 'pages/contact'
+  #alternate way
+  #get controller/action
+
+  get 'about', to: 'pages#about'
+  get 'contact', to: 'pages#contact'
+
+
+  #we can have custom ones even if we dont have the same file name
+
+  get 'about-me', to: 'pages#about'
+
+
+
   resources :blogs
 
+  root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
