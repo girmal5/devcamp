0b7ed881a5dac66c4fd1ab8571e947cc4cc0e719
girmal <girmal5@hotmail.com>
2018-05-28 17:24:06 +0530

    Implemented alternate way for setting defaults this method was via .rb file in our case image defaults

--- app/models/pfolio.rb
+++ app/models/pfolio.rb
@@ -4,4 +4,13 @@ class Pfolio < ApplicationRecord
 
   scope :angularfilter, -> {where(subtitle: "Angular")}
 
+
+#right after pfolio.new has run this procoess auto runs
+  after_initialize :set_defaults
+
+  def set_defaults
+    self.main_image ||= "http://via.placeholder.com/600x400"
+    self.thumb_image ||= "http://via.placeholder.com/350x150"
+  end
+
 end
