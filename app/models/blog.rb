6d7c36dcb13a0714e261cf247637241718cbe63b
girmal <girmal5@hotmail.com>
2018-05-28 13:18:46 +0530

    Deep Dive routes

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,6 +1,10 @@
 class Blog < ApplicationRecord
   #by default will be in draft mode, otherwise published
-  #can then be used as a method e.g in console do Blog.last.published! 
+  #can then be used as a method e.g in console do Blog.last.published!
+  #we can then also use logic as true and fale when we do Blog.last.published? it returns true
+  #or false then can use that shizzz
+
+  
   enum status: {draft: 0, published: 1}
   extend FriendlyId
   friendly_id :title, use: :slugged
