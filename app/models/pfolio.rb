ef7b783de8f9da5aee9e1f02d6b2a80620108939
girmal <girmal5@hotmail.com>
2018-05-28 18:28:54 +0530

    Learnt how to use concerns, implemented helper method, in our case for managing image placeholders

--- app/models/pfolio.rb
+++ app/models/pfolio.rb
@@ -1,4 +1,6 @@
 class Pfolio < ApplicationRecord
+  include Placeholder
+
   validates_presence_of :title,:body,:main_image,:thumb_image
 
 
@@ -9,8 +11,11 @@ class Pfolio < ApplicationRecord
   after_initialize :set_defaults
 
   def set_defaults
-    self.main_image ||= "http://via.placeholder.com/600x400"
-    self.thumb_image ||= "http://via.placeholder.com/350x150"
+    self.main_image ||= Placeholder.image_generator(height: '600',width: '400')
+    self.thumb_image ||= Placeholder.image_generator(height: '350',width: '150')
   end
+
+  #using just = equals to will overwrite existing
+  #using ||= is like writing if image is nill then put else dont
 
 end
