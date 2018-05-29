c65d75c4aad32d75bec5b1128983d073afe20dd3
girmal <girmal5@hotmail.com>
2018-05-29 16:48:16 +0530

    Customized reg login and logout routes to our own routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, path: '', path_names: {sign_in: 'login', sign_out: 'logout',sign_up: 'register'}
+
+
   #bring all the resource routes for pfolios except show
   resources :pfolios, except: [:show]
 
