c76afcfbb7a5fa8e46ed7664f8d381627ab6e3a7
girmal <girmal5@hotmail.com>
2018-05-29 15:40:22 +0530

    Integrated Nested Attributes for Pfolio model

--- app/models/pfolio.rb
+++ app/models/pfolio.rb
@@ -1,9 +1,18 @@
 class Pfolio < ApplicationRecord
   has_many :technologies
+  accepts_nested_attributes_for :technologies, reject_if: lambda { |attrs| attrs['name'].blank?}
+
   include Placeholder
 
   validates_presence_of :title,:body,:main_image,:thumb_image
 
+
+
+#2 ways of creating custom methods
+
+  def self.angfilter
+    where(subtitle: 'Angular')
+  end
 
   scope :angularfilter, -> {where(subtitle: "Angular")}
 
@@ -16,7 +25,9 @@ class Pfolio < ApplicationRecord
     self.thumb_image ||= Placeholder.image_generator(height: '350',width: '150')
   end
 
+
   #using just = equals to will overwrite existing
   #using ||= is like writing if image is nill then put else dont
+
 
 end
