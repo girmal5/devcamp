fd37e95586153432c971e1aa16cc76d8e6bd924e
girmal <girmal5@hotmail.com>
2018-05-30 19:25:34 +0530

    Completed authentication with virtual attributes such as first_name last_name for user model in USER.RB

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,19 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+
+  validates_presence_of :name
+
+  def first_name
+    self.name.split.first
+    #can also write as self.name.split[0]
+  end
+
+  def last_name
+    #can also write as self.name.split[1]
+    self.name.split.last
+  end
+
+
 end
